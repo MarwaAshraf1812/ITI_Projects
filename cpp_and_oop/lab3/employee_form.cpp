807c8c33804ac9078292e9dd4c6ea3ce0a385104
MarwaAshraf1812 <marashraf090@gmail.com>
2025-11-06 16:29:43 +0200

    Refactor employee form to use struct for employee data and streamline input handling

--- cpp_and_oop/lab3/employee_form.cpp
+++ cpp_and_oop/lab3/employee_form.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+
 using namespace std;
+
+struct employee
+{
+  string name;
+  double salary;
+};
 
 int main()
 {
   system("clear");
-  string emp_name;
-  double emp_salary;
+  employee emp[5];
 
   cout << "\033[1;33m==== EMPLOYEE FORM ====\033[0m\n\n";
+
   cout << "\033[3;10HEnter employee name: \033[3;35H";
-  cin >> emp_name;
-
-  cout << "\033[3;10HEnter employee salary: \033[3;35H";
-  cin >> emp_salary;
-
-  cout << "\033[1;32mEmployee data saved successfully!\033[0m\n\n";
+  cout << "\033[4;10HEnter employee salary: \033[4;35H";
 
 
+  cout << "\033[3;35H\033[K"; 
+  cin >> emp[0].name;
 
-    cout << "\033[1;32mEmplyee data \033[0m\n\n";
-    cout << "Name: " << emp_name << "\n";
-    cout << "Salary: " << emp_salary << "\n";
+  cout << "\033[4;35H\033[K"; 
+  cin >> emp[0].salary;
+
+
+  cin.ignore();
+  cout << "\033[5;10H----------------------------------------------\n";
+
+
+  cout << "\n\n\033[1;32mEmployee data saved successfully!\033[0m\n\n";
+
+  cout << "\033[1;32mEmployee data \033[0m\n\n";
+
+    cout << "\033[0;36m " << "Name:  \033[0m" << emp[0].name << "\n";
+    cout << "\033[0;36m " << "Salary:  \033[0m" << emp[0].salary << "\n";
+    cout << "-----------------------\n";
 
   return 0;
 }
