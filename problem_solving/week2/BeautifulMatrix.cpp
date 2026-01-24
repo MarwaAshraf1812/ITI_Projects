b232716cb2f8441aa9bae28a2f6258ebc8a600dc
MarwaAshraf1812 <marashraf090@gmail.com>
2026-01-25 00:14:14 +0200

    beautiful matrix problem

--- problem_solving/week2/BeautifulMatrix.cpp
+++ problem_solving/week2/BeautifulMatrix.cpp
@@ -6,17 +6,16 @@ int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  int matrix[5][5];
   int count = 0;
+  int value;
   for(int i=0; i<5; i++) {
     for(int j=0; j<5; j++) {
-      cin>>matrix[i][j];
-      if(matrix[i][j] == 1) {
-        
+      cin>>value;
+      if(value == 1) {
+        count = abs(i - 2) + abs(j - 2);
       }
     }
   }
-
-
+  cout<<count <<endl;
   return 0;
 }
