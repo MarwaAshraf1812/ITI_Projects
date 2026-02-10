b0511cc693eecb23c632dbbb167b53da1815d664
MarwaAshraf1812 <marashraf090@gmail.com>
2026-02-10 08:36:53 +0200

    update al files and design

--- RWD/day3/FlowerBlog/script.js
+++ RWD/day3/FlowerBlog/script.js
@@ -1,19 +1,11 @@
-const toggleBtn = document.getElementById("theme-icon");
-const themeElement = document.body;
+const toggle = document.getElementById("theme-toggle");
+    const html = document.documentElement;
 
-  toggleBtn.addEventListener("click", () => {
-    const currentTheme = themeElement.getAttribute("data-bs-theme");
-    
-    const newTheme = currentTheme === "light" ? "dark" : "light";
-    themeElement.setAttribute("data-bs-theme", newTheme);
+    toggle.addEventListener("click", () => {
+      const current = html.getAttribute("data-bs-theme");
+      const next = current === "light" ? "dark" : "light";
 
-    if (newTheme === "dark") {
-      toggleBtn.classList.remove("bi-moon-stars-fill");
-      toggleBtn.classList.add("bi-sun-fill");
-      toggleBtn.classList.replace('text-light', 'text-warning');
-    } else {
-      toggleBtn.classList.remove("bi-sun-fill");
-      toggleBtn.classList.add("bi-moon-stars-fill");
-      toggleBtn.classList.replace('text-warning', 'text-light');
-    }
-  });
+      html.setAttribute("data-bs-theme", next);
+      toggle.classList.toggle("bi-moon-stars-fill");
+      toggle.classList.toggle("bi-sun-fill");
+    });
